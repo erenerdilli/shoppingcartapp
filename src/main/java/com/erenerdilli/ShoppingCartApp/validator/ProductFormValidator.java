c4cdf60d3e5194c110334af915667f0f6040ddd9
Earendilli <erenerdilli@gmail.com>
2020-11-03 19:55:26 +0300

    Implemented Form Validators

---
+++ src/main/java/com/erenerdilli/ShoppingCartApp/validator/ProductFormValidator.java
@@ -0,0 +1,43 @@
+package com.erenerdilli.ShoppingCartApp.validator;
+
+import com.erenerdilli.ShoppingCartApp.dao.ProductDAO;
+import com.erenerdilli.ShoppingCartApp.entity.Product;
+import com.erenerdilli.ShoppingCartApp.form.ProductForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class ProductFormValidator implements Validator {
+
+    @Autowired
+    private ProductDAO productDAO;
+
+    // This validator only checks for the ProductForm.
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz == ProductForm.class;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ProductForm productForm = (ProductForm) target;
+
+        // Check the fields of ProductForm.
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "code", "NotEmpty.productForm.code");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "NotEmpty.productForm.name");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "price", "NotEmpty.productForm.price");
+
+        String code = productForm.getCode();
+        if (code != null && code.length() > 0) {
+            if (code.matches("\\s+")) {
+                errors.rejectValue("code", "Pattern.productForm.code");
+            } else if (productForm.isNewProduct()) {
+                Product product = productDAO.findProduct(code);
+                if (product != null) {
+                    errors.rejectValue("code", "Duplicate.productForm.code");
+                }
+            }
+        }
+    }
+}
